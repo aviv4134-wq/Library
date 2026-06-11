075f8f88f9103e3035c8c7d4b15f117ef19f5794
avraham <aviv4134@gmail.com>
2026-06-11 16:06:02 +0300

    function/create table

--- database/db_connection.py
+++ database/db_connection.py
@@ -11,10 +11,33 @@ def get_connection():
     )
     return conn
 
-    
+def create_table():
+    conn = get_connection()
+    curs = conn.cursor()
+    query = '''
+    CREATE TABLE IF NOT EXISTS books(
+     id           INT         AUTO_INCREMENT PRIMARY KEY,  
+     title        VARCHAR(50)   NOT NULL,
+     author       VARCHAR(50)   NOT NULL,
+     genre        ENUM(Fiction, Non-Fiction, Science, History, Other)  NOT NULL,  
+     is_available BOOLEAN    NOT NULL,                                        
+     id_member_by_borrowed    INT        DEFAULT NULL                          
+                      );
+                      '''
+    curs.execute('''CREATE TABLE IF NOT EXISTS books(
+         id           INT         AUTO_INCREMENT PRIMARY KEY,  
+         title        VARCHAR(50)   NOT NULL,
+         author       VARCHAR(50)   NOT NULL,
+         genre        ENUM(Fiction, Non-Fiction, Science, History, Other)  NOT NULL,  
+         is_available             BOOLEAN    NOT NULL,                                        
+         id_member_by_borrowed    INT        DEFAULT NULL  
+                 )''')
 
-
-
+# conn = get_connection()
+# curs = conn.cursor()
+# curs.execute('SHOW DATABASES;')
+# print(curs.fetchall())
+create_table()
 
 
 
