5571503bda2c0e1cbee85f19a104857d1470a54d
avraham <aviv4134@gmail.com>
2026-06-11 20:23:34 +0300

    function/create tables completed

--- database/db_connection.py
+++ database/db_connection.py
@@ -11,33 +11,35 @@ def get_connection():
     )
     return conn
 
-def create_table():
-    conn = get_connection()
-    curs = conn.cursor()
-    query = '''
-    CREATE TABLE IF NOT EXISTS books(
-     id           INT         AUTO_INCREMENT PRIMARY KEY,  
-     title        VARCHAR(50)   NOT NULL,
-     author       VARCHAR(50)   NOT NULL,
-     genre        ENUM(Fiction, Non-Fiction, Science, History, Other)  NOT NULL,  
-     is_available BOOLEAN    NOT NULL,                                        
-     id_member_by_borrowed    INT        DEFAULT NULL                          
-                      );
-                      '''
-    curs.execute('''CREATE TABLE IF NOT EXISTS books(
-         id           INT         AUTO_INCREMENT PRIMARY KEY,  
-         title        VARCHAR(50)   NOT NULL,
-         author       VARCHAR(50)   NOT NULL,
-         genre        ENUM(Fiction, Non-Fiction, Science, History, Other)  NOT NULL,  
-         is_available             BOOLEAN    NOT NULL,                                        
-         id_member_by_borrowed    INT        DEFAULT NULL  
-                 )''')
-
-# conn = get_connection()
-# curs = conn.cursor()
-# curs.execute('SHOW DATABASES;')
-# print(curs.fetchall())
-create_table()
-
+def create_table() -> str:
+        try:
+            conn = get_connection()
+            curs = conn.cursor()
+            query_create_books_table = '''
+            CREATE TABLE IF NOT EXISTS books(
+                    id           INT         AUTO_INCREMENT PRIMARY KEY,  
+                    title        VARCHAR(50)   NOT NULL,
+                    author       VARCHAR(50)   NOT NULL,
+                    genre        ENUM('Fiction','Non-Fiction','Science','History','Other')  NOT NULL,  
+                    is_available             BOOLEAN    NOT NULL,                                        
+                    id_member_by_borrowed    INT        DEFAULT NULL  
+                            )'''
+            query_create_members_table = '''
+                CREATE TABLE IF NOT EXISTS members(
+                id          INT         AUTO_INCREMENT PRIMARY KEY,  
+                name        VARCHAR(50)   NOT NULL,
+                email       VARCHAR(255)     NOT NULL   UNIQUE,
+                is_active   BOOLEAN      NOT NULL,                                        
+                borrows_total  INT       NOT NULL                    
+                                )'''                
+            curs.execute(query_create_books_table)
+            curs.execute(query_create_members_table)
+            return 'the tables created '
+        except Exception as error:
+             return f'error not created \n {error}'            
 
 
+
+if __name__  == '__main__':
+    print(get_connection())
+    print(create_table())
